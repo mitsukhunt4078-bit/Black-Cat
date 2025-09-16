2d2c54786f2fee8778c4edbfafd838d10f0be37f
Mitsu Khunt <mitsu.khunt.sod24@anu.edu.in>
2025-09-16 16:39:20 +0530

    added aall files
    yay!!!!

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(20,50,150);
+  
+  //Draw the matchstick
+  fill(120,80,20)
+  rect(215,250,30,150)
+  
+  //Draw the elipse
+  fill(255,165,0)
+  ellipse(230,170,140,250)
+  
+  //Draw the outer circle
+  fill(255,255,0)
+  circle(230,255,80)
+  
+  
+  fill(255,255,150)
+  circle(230,270,50)
+  
+ 
+  }
